846e01069aaee62883323df6b97f413f2368f6d0
edita21 <ekuberk0@chicagobooth.edu>
2021-07-24 20:37:20 +0000

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
+
+  get("/directors", { :controller => "directors", :action => "index"})
+
+  get("/directors/youngest", { :controller => "directors", :action => "youngest"})
+
+  get("directors/eldest", { :controller => "directors", :action => "eldest"})
+
 end
